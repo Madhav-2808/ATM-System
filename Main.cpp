b09de5f5ec2a76d5bf1a109dfd671c6131f8e646
Madhav Singh <113347221+Madhav-2808@users.noreply.github.com>
2023-06-07 00:05:40 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,17 @@
 #include<string>
 using namespace std;
 
-class ATM{                  // class ATM declaration
-  private:										// private member varibles
+class ATM{                	    // class ATM declaration
+	private:		    // member varibles defined with private access modifier
 	long int Acc_No;
-  string Mobile_No;
+  	string Mobile_No;
 	string Name;
 	int PIN;
 	double Balance;
 	
-}
+	
+	public:	                      // member functions defined with public access modifier
+
+	
+
+};
