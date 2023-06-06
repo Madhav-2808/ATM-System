a800d7441946d1f4e16a22a4e56d8f0a07f79460
Madhav Singh <113347221+Madhav-2808@users.noreply.github.com>
2023-06-07 00:02:12 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,3 +2,13 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class ATM{                  // class ATM declaration
+  private:										// private member varibles
+	long int Acc_No;
+  string Mobile_No;
+	string Name;
+	int PIN;
+	double Balance;
+	
+}
