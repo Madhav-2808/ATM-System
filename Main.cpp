23fe6514cea62467a2f7e532baed44b8a0decdde
Madhav Singh <113347221+Madhav-2808@users.noreply.github.com>
2023-06-27 20:59:51 +0530

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,12 @@ class ATM{                	    // class ATM declaration
 	public:	                      // member functions defined with public access modifier
 	
 	// setData function is setting the Data into the private member variables
-	void setData(){
+	void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a){
+		account_No = account_No_a;	
+		name = name_a;
+		PIN = PIN_a;
+		balance = balance_a;
+		mobile_No = mobile_No_a;
 	}
 	//getAccountNo function is returning the user's account no.
 	long int getAccountNo(){
@@ -38,45 +43,6 @@ class ATM{                	    // class ATM declaration
    -> Update Mobile No.
 */
 
-
-class atm										// class atm 
-{
-private:										// private member varibles
-	long int account_No;
-	string name;
-	int PIN;
-	double balance;
-	string mobile_No;
-
-public:											// public member functions
-
-	// setData function is setting the Data into the private member variables
-	void setData(long int account_No_a, string name_a, int PIN_a, double balance_a, string mobile_No_a)
-	{
-		account_No = account_No_a;	 // assigning the formal arguments to the private member var's
-		name = name_a;
-		PIN = PIN_a;
-		balance = balance_a;
-		mobile_No = mobile_No_a;
-	}
-
-	//getAccountNo function is returning the user's account no.
-	long int getAccountNo()
-	{
-		return account_No;
-	}
-	
-	//getName function is returning the user's Name
-	string getName()
-	{
-		return name;
-	}
-
-	//getPIN function is returning the user's PIN
-	int getPIN()
-	{
-		return PIN;
-	}
 
 	//getBalance is returning the user's Bank Balance
 	double getBalance()
@@ -129,8 +95,7 @@ public:											// public member functions
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-int main()
-{
+int main(){
 	int choice = 0, enterPIN;		//enterPIN and enterAccountNo. ---> user authentication
 	long int enterAccountNo;
 
